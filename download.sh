6f00cdc522f44b2d22b4bd733a6da704232f0675
eggplants <w10776e8w@yahoo.co.jp>
2021-07-27 05:00:01 +0900

    fix

--- download.sh
+++ download.sh
@@ -6,7 +6,7 @@ readonly ALTKDB_REPO="Make-IT-TSUKUBA/alternative-tsukuba-kdb"
 function _get_latest_csv() {
   curl -s -H "Accept: application/vnd.github.v3+json" \
     "https://api.github.com/repos/${ALTKDB_REPO}/git/trees/master?recursive=1" |
-    grep -oP '(?<="path": ")csv/[^"]+\.csv' | sort | tail -1
+    tac | grep -oPm1 '(?<="path": ")csv/[^"]+\.csv'
 }
 
 # Obtaining a list of subject codes from the latest syllabus data.
@@ -14,9 +14,10 @@ function _get_code_list() {
   local LATEST_CSV YEAR
   readonly LATEST_CSV="$(_get_latest_csv)"
   readonly YEAR="${LATEST_CSV:8:4}"
-  echo "[codes]:">&2
+  echo -e "[latest]:\n${LATEST_CSV}\n[year]:\n${YEAR}">&2
+  echo "[urls]:">&2
   curl -sL "https://github.com/${ALTKDB_REPO}/raw/master/${LATEST_CSV}" |
-  awk -F'[,"]' 'NR>1&&$2!=""{print"https://kdb.tsukuba.ac.jp/syllabi/"y"/"$2"/jpn"}' y="$YEAR"
+  awk -F'[,"]' 'NR>1&&$2!=""{print"https://kdb.tsukuba.ac.jp/syllabi/"y"/"$2"/jpn/"}' y="$YEAR"
 }
 
 # Download stdin urls to received from optional cmdarg.
@@ -30,7 +31,7 @@ function _download() {
     [ -n "$class_code" ] \
       && wget -O "${DEST}/${class_code}.html" \
               --cipher 'DEFAULT:!DH' -q \
-              -w3 --random-wait \
+              -w 3 --random-wait \
               "$url" && echo "=>OK" || echo "=>NG"
   done
 }
